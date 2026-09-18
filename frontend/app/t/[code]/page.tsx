90a80376100ecc8e34b35ce57ef30a7828100577
Frank Koine <91139887+PrinceKeldon@users.noreply.github.com>
2026-09-18 18:08:14 +0200

    Load thing photos through backend proxy

--- frontend/app/t/[code]/page.tsx
+++ frontend/app/t/[code]/page.tsx
@@ -41,7 +41,7 @@ function KnownThing({ thing }: { thing: Thing }) {
     <Centered>
       {primaryPhoto && (
         <img
-          src={mediaUrl(primaryPhoto.url)}
+          src={mediaUrl('/things/' + thing.onekey_code + '/photo')}
           alt={thing.name}
           style={{ width: "100%", borderRadius: 12, marginBottom: "1rem", objectFit: "cover", maxHeight: 320 }}
         />
